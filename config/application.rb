66e6fb499f58266a1ddc7bbfb4f8b5dfcdae37a3
Rachel Isaac <rachelisaac995@gmail.com>
2023-05-29 17:29:59 +0400

    Revert "Set up Linters"
    This reverts commit 415b9437b59415b4647a65c3c469d9600eb47e92.

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,8 @@
-# frozen_string_literal: true
-
 require 'dotenv/load'
 
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
